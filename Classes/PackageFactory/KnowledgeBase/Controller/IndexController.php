1354b2ef8eee25d5951f4bf2a1161aac227c4e6d
Wilhelm Behncke <wilhelm.behncke@googlemail.com>
2015-04-28 20:47:45 +0200

    [WIP][TASK] Setup base template
    References: #114
    Releases: master

---
+++ Classes/PackageFactory/KnowledgeBase/Controller/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+namespace PackageFactory\KnowledgeBase\Controller;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "PackageFactory.KnowledgeBase".*
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+class IndexController extends \TYPO3\Flow\Mvc\Controller\ActionController {
+
+	/**
+	 * @return void
+	 */
+	public function indexAction() {
+		
+	}
+
+}
